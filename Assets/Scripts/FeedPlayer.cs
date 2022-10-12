3e810aab3ee400be7afa01f43bd00b56b6bb7932
AllFatherGray <16143672+AllFatherGray@users.noreply.github.com>
2022-10-12 15:33:11 -0400

    Required components and initialization
    Added RequireComponent to FeedPlayer to require both MixingAudioPlayer and Session components as they will always be need.
    
    Using the Unity Editor's lifecycle, we also made the binding happen automatically. As soon the FeedPlayer component is added, the references to MixingAudioPlayer and Session are initialized. This makes using the SDK much smoother on the editor side. The user of the SDK now only has to add FeedPlayer and all else is handled by FeedFM.
    
    The binding of the two required scripts will be done during compile time as oppose to the previous SDK which was do during runtime. This change has removed the memory fragmentation that exists in the previous SDK.
    
    DisallowMultipleComponent has been added to FeedPlayer, MixingAudioPlayer, and Session as only one of each component is required to create a working set.
    
    For legacy compatibility, we move FeedPlayer up twice to maintain the order below
    - FeedPlayer
    - MixingAudioPlayer
    - Session
    This will allow for a smoother migration for users of the previous SDK who have code that depends on the order of initialization.

---
+++ Assets/Scripts/FeedPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace FeedFM
+{
+    [RequireComponent(typeof(MixingAudioPlayer))]
+    [RequireComponent(typeof(Session))]
+    [DisallowMultipleComponent]
+    internal sealed class FeedPlayer : MonoBehaviour
+    {
+        [SerializeField] private MixingAudioPlayer _mixingAudioPlayer = null;
+        [SerializeField] private Session _session = null;
+        
+        private void Awake()
+        {
+            InitializeRequiredComponents();
+        }
+
+        private void InitializeRequiredComponents()
+        {
+            if (!_mixingAudioPlayer)
+            {
+                _mixingAudioPlayer = GetComponent<MixingAudioPlayer>();
+                if (!_mixingAudioPlayer)
+                {
+                    _mixingAudioPlayer = gameObject.AddComponent<MixingAudioPlayer>();
+                }
+            }
+
+            if (!_session)
+            {
+                _session = GetComponent<Session>();
+                if (!_session)
+                {
+                    _session = gameObject.AddComponent<Session>();
+                }
+            }
+        }
+
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            UnityEditorInternal.ComponentUtility.MoveComponentUp(this);
+            UnityEditorInternal.ComponentUtility.MoveComponentUp(this);
+            
+            InitializeRequiredComponents();
+        }
+#endif
+    }
+}
