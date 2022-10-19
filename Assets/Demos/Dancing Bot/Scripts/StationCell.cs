ccce8d6a4763a6cc2a2820267c99dd45266cbbd9
AllFatherGray <16143672+AllFatherGray@users.noreply.github.com>
2022-10-19 10:05:20 -0400

    Dancing Bot Demo feature parity WIP

---
+++ Assets/Demos/Dancing Bot/Scripts/StationCell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FeedFM.Models;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FeedFM.Demos.Dancing_Bot.Scripts
+{
+    internal class StationCell : MonoBehaviour
+    {
+        public static event Action<StationCell> OnButtonTapped = null;
+        
+        [SerializeField] private TextMeshProUGUI _stationName = null;
+        [SerializeField] private Button _button = null;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(Tapped);
+        }
+
+        public void Initialize(Station station)
+        {
+            _stationName.text = station.Name;
+        }
+
+        private void Tapped()
+        {
+            OnButtonTapped?.Invoke(this);
+        }
+    }
+}
