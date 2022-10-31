8ffdc00716c8da3eec786d57348781d37740fca6
Ethan Weibman <themadarchitect@users.noreply.github.com>
2022-10-31 11:18:54 -0400

    Fixes (#18)
    * Update README.md
    
    * Put Readonly attributes behind Unity_Editor macros
    
    * Removed legacy timed intervals.
    
    Dropped the half second interval from the legacy sdk was would cause the current play time to never reach the current play duration. This was preventing autoplay on some songs.
    
    Co-authored-by: AllFatherGray <16143672+AllFatherGray@users.noreply.github.com>

--- Scripts/Attributes/Editor/ReadOnlyDuringPlayAttributeDrawer.cs
+++ Scripts/Attributes/Editor/ReadOnlyDuringPlayAttributeDrawer.cs
@@ -1,19 +1,24 @@
 ﻿using FeedFM.Attributes;
 using UnityEngine;
 using UnityEditor;
- 
-[CustomPropertyDrawer(typeof(ReadOnlyDuringPlayAttribute))]
-public class ReadOnlyDuringPlayAttributeDrawer : PropertyDrawer
+
+namespace FeedFM.Attributes.Editor
 {
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(ReadOnlyDuringPlayAttribute))]
+    public class ReadOnlyDuringPlayAttributeDrawer : PropertyDrawer
     {
-        return EditorGUI.GetPropertyHeight(property, label, true);
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = !Application.isPlaying;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
     }
- 
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        GUI.enabled = !Application.isPlaying;
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
-    }
+#endif
 }
