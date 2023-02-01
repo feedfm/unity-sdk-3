908e13c3bc844a4b7e040b8381249289c9f29499
Arveen <arvkmr@users.noreply.github.com>
2023-02-01 17:31:58 +0000

    fix(version): deploy new version

--- Assets/Scripts/Attributes/ReadOnlyAttribute.cs
+++ Assets/Scripts/Attributes/ReadOnlyAttribute.cs
@@ -6,6 +6,6 @@ namespace FeedFM.Attributes
     [AttributeUsage(AttributeTargets.Field)]
     public class ReadOnlyAttribute : PropertyAttribute
     {
-        public const string VERSION = "0.0.14";
+        public const string VERSION = "1.0.1";
     }
 }
